0715c2a769c0e54948044f66d738d6fd74d44830
Patricio Mariano <patomdp00@gmail.com>
2024-10-22 21:35:20 +0200

    before update to v18

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
+import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirecciona a login por defercto
+  { path: 'login', component: LoginComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'recuperar-password', component: RecuperarPasswordComponent},
+  { path: 'registrar-usuario', component: RegistrarUsuarioComponent},
+  { path: 'verificar-correo', component: VerificarCorreoComponent},
+  { path: '**', redirectTo: 'login', pathMatch: 'full' } // poner siempre al fondo del array
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
